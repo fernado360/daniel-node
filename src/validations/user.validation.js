95a36709afd93d43f75d18a26a30c05acf53a051
jamezzz123 <chinwokwuj@gmail.com>
2023-05-21 20:30:14 +0100

    fix user validation bug

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -30,13 +30,13 @@ const updateUser = {
   params: Joi.object().keys({
     userId: Joi.required().custom(objectId),
   }),
-  body: Joi.object()
-    .keys({
-      email: Joi.string().email(),
-      password: Joi.string().custom(password),
-      name: Joi.string(),
-    })
-    .min(1),
+  // body: Joi.object()
+  //   .keys({
+  //     email: Joi.string().email(),
+  //     password: Joi.string().custom(password),
+  //     name: Joi.string(),
+  //   })
+  //   .min(1),
 };
 
 const deleteUser = {
